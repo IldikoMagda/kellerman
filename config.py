a1c3fb2d7999e4498a9e219c1bcff8b16267b213
IldikoMagda <45874110+IldikoMagda@users.noreply.github.com>
2019-01-21 00:22:38 +0000

    Delete config.py

--- config.py
+++
@@ -1,3 +0,0 @@
-DEBUG = True    # Turns on debugging features in Flask
-BCRYPT_LOG_ROUNDS = 12  # Configuration for the Flask-Bcrypt extension --see more in whatidid.txt
-MAIL_FROM_EMAIL = "ildiko.magda@gmail.com" # For use in application emails
