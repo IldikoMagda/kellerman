ef01dacd5d338bcd01d9a85f15a754fa878564f7
lydstarr <lydss94@gmail.com>
2019-01-22 21:52:11 +0000

    Oky

--- run.py
+++ run.py
@@ -1,2 +1,9 @@
-from flask import Flask  # this file gets a copy of the app code, as versions sort of
-app = Flask(__name__)
+import os
+
+from app import app
+
+port = int(os.environ.get('PORT', 33506))
+# to run local
+#app.run(debug=app.config['DEBUG'], use_reloader=app.config['RELOADER'], port=port)
+# run deployed
+app.run(debug=app.config['DEBUG'], use_reloader=app.config['RELOADER'], port=port, host='0.0.0.0')
