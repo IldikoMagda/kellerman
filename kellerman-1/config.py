03bca566527601f9c975de5dd91b5df3c15abec9
lydstarr <lydss94@gmail.com>
2019-01-23 10:54:55 +0000

    hey

---
+++ kellerman-1/config.py
@@ -0,0 +1,5 @@
+""" Define All essential configurations we will need for the app itself"""
+
+DEBUG = True    # Turns on debugging features in Flask
+BCRYPT_LOG_ROUNDS = 12  # Configuration for the Flask-Bcrypt extension --see more in whatidid.txt
+RELOADER = True
