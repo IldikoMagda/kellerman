894184f9a8761b6d5cf2710ec06f00048a10f6b6
IldikoMagda <ildiko.magda@gmail.com>
2019-01-24 15:37:45 +0000

    Revert to working version

--- run.py
+++ run.py
@@ -1,21 +1,9 @@
-<<<<<<< HEAD
+""" This File is used to run the app deployed or locally"""
 import os
-
-from app import app
+from app import APP
 
 port = int(os.environ.get('PORT', 33506))
 # to run local
-app.run(debug=app.config['DEBUG'], use_reloader=app.config['RELOADER'], port=port)
+#APP.run(debug=APP.config['DEBUG'], use_reloader=APP.config['RELOADER'])
 # run deployed
-#app.run(debug=app.config['DEBUG'], use_reloader=app.config['RELOADER'], port=port, host='0.0.0.0')
-=======
-import os
-
-from app import app
-
-port = int(os.environ.get('PORT', 33506))
-# to run local
-#app.run(debug=app.config['DEBUG'], use_reloader=app.config['RELOADER'], port=port)
-# run deployed
-app.run(debug=app.config['DEBUG'], use_reloader=app.config['RELOADER'], port=port, host='0.0.0.0')
->>>>>>> 10b9717a694bb439ed9c3fe20f828671c6d57e3e
+APP.run(debug=APP.config['DEBUG'], use_reloader=APP.config['RELOADER'], port=port, host='0.0.0.0')
