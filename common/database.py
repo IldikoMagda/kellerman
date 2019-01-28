c394b4d7ed18124990b3177fe8cc9f00af81f833
lydstarr <44969285+lydstarr@users.noreply.github.com>
2019-01-28 13:06:27 +0000

    Update database.py

--- common/database.py
+++ common/database.py
@@ -2,21 +2,25 @@ import os
 import psycopg2
 
 def Query(query):
+
     # Define our connection string
     conn_string = os.getenv('DB_CONNECTION')
-    # get a connection, if a connect cannot be made an exception will be raised here
-    conn = psycopg2.connect("dbname=kellerman-db")
+    print(conn_string)
+    try:
+        # get a connection, if a connect cannot be made an exception will be raised here
+        conn = psycopg2.connect(conn_string)
 
-    # conn.cursor will return a cursor object, you can use this cursor to perform queries
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM public.kinase;')
-    query = cursor.fetchall()
+        # conn.cursor will return a cursor object, you can use this cursor to perform queries
+        cursor = conn.cursor()
+        cursor.execute(query)
 
-    cursor.close()
-    conn.close()
+        result = cursor.fetchall()
+        conn.close()
         # print("Print each row and it's columns values")
         # for row in result:
         #     print("Id = ", row[0], )
         #     print("Name = ", row[1])
         #     print("Family  = ", row[2], "\n")
-    return query
+        return result 
+    except:
+        return 'An error occured while executing SQL query'
