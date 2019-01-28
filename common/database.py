f32f41b999771fa2b4bbfbfbb14c97f236155da9
lydstarr <44969285+lydstarr@users.noreply.github.com>
2019-01-28 12:46:46 +0000

    Update database.py

--- common/database.py
+++ common/database.py
@@ -5,7 +5,7 @@ def Query(query):
     # Define our connection string
     conn_string = os.getenv('DB_CONNECTION')
     # get a connection, if a connect cannot be made an exception will be raised here
-    conn = psycopg2.connect('')
+    conn_string = psycopg2.connect('')
 
     # conn.cursor will return a cursor object, you can use this cursor to perform queries
     cursor = conn.cursor()
