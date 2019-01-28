e970b85cee2501a1945a15d5ff59b7199bfbfa02
lydstarr <44969285+lydstarr@users.noreply.github.com>
2019-01-28 16:09:21 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,32 +1,5 @@
 """ Define All essential configurations we will need for the app itself"""
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-
-class Config(object):
-    DEBUG = False
-    TESTING = False
-    CSRF_ENABLED = True
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-
-
-class ProductionConfig(Config):
-    DEBUG = False
-
-
-class StagingConfig(Config):
-    DEVELOPMENT = True
-    DEBUG = True
-
-
-class DevelopmentConfig(Config):
-    DEVELOPMENT = True
-    DEBUG = True
-
-
-class TestingConfig(Config):
-    TESTING = True
-    
+  
     
 DEBUG = True    # Turns on debugging features in Flask
 BCRYPT_LOG_ROUNDS = 12  # Configuration for the Flask-Bcrypt extension --see more in whatidid.txt
