60d29f7d1332bfb5d9e9f025e4b4b32a39a99eb8
lydstarr <44969285+lydstarr@users.noreply.github.com>
2019-01-28 10:50:01 +0000

    Update database.py

--- common/database.py
+++ common/database.py
@@ -19,3 +19,26 @@ except:
     return 'An error occured while executing SQL query'
 
 
+def Query(query):
+
+    # Define our connection string
+    conn_string = os.getenv('DB_CONNECTION')
+    print(conn_string)
+    try:
+        # get a connection, if a connect cannot be made an exception will be raised here
+        conn = psycopg2.connect(conn_string)
+
+        # conn.cursor will return a cursor object, you can use this cursor to perform queries
+        cursor = conn.cursor()
+        cursor.execute(query)
+
+        result = cursor.fetchall()
+        conn.close()
+        # print("Print each row and it's columns values")
+        # for row in result:
+        #     print("Id = ", row[0], )
+        #     print("Name = ", row[1])
+        #     print("Family  = ", row[2], "\n")
+        return result
+    except:
+        return 'An error occured while executing SQL query'
