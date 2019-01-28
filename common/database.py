310555bfd7fce9f0024647705c9685ef3b7b72a8
lydstarr <44969285+lydstarr@users.noreply.github.com>
2019-01-28 12:23:21 +0000

    Update database.py

--- common/database.py
+++ common/database.py
@@ -2,23 +2,21 @@ import os
 import psycopg2
 
 def Query(query):
-
     # Define our connection string
     conn_string = os.getenv('DB_CONNECTION')
-    print(conn_string)
-    try:
-        # get a connection, if a connect cannot be made an exception will be raised here
-        conn = psycopg2.connect(conn_string)
+    # get a connection, if a connect cannot be made an exception will be raised here
+    conn = psycopg2.connect(conn_string)
 
-        # conn.cursor will return a cursor object, you can use this cursor to perform queries
-        cursor = conn.cursor()
-        cursor.execute('SELECT * FROM public.kinase;')
+    # conn.cursor will return a cursor object, you can use this cursor to perform queries
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM public.kinase;')
+    query = cursor.fetchall()
 
-        data = cursor.fetchall()
-        conn.close()
+    cursor.close()
+    conn.close()
         # print("Print each row and it's columns values")
         # for row in result:
         #     print("Id = ", row[0], )
         #     print("Name = ", row[1])
         #     print("Family  = ", row[2], "\n")
-        return data
+    return query
