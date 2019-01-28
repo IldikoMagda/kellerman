76114075c4ec8347f3c60bbafe1f21edd983b995
lydstarr <44969285+lydstarr@users.noreply.github.com>
2019-01-28 12:15:44 +0000

    Update views.py

--- models/kinase/views.py
+++ models/kinase/views.py
@@ -5,11 +5,14 @@ import common.database as db
 
 kinase_blueprint = Blueprint('kinase', __name__)
 @kinase_blueprint.route('/', methods=['GET', 'POST'])
-def index():
-    kinase_name=None   
-    if request.method == 'POST':
-        kinase_name= request.form['name']
-        query = 'SELECT kinase_id, kinase_name, family, inhibitor FROM public.kinase'
-    elif request.method == 'GET':
+def index():   
+    query = 'SELECT kinase_id, kinase_name, family, inhibitor FROM public.kinase'
+    if request.method == 'GET':
+        data = db.Query(query)
+    else:
+        if request.form['name']:
+            nameFilter = request.form['name']
+            query = query + " WHERE kinase_name LIKE '%" + nameFilter + "%'"
+            print('Query: ' + query)
         data = db.Query(query)
     return render_template('kinase/index.html', data=data, content_type='application/json')
