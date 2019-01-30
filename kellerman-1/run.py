928241e6ec61700552e375588e03ebe718869e27
lydstarr <lydss94@gmail.com>
2019-01-30 16:43:40 +0000

    delete

--- kellerman-1/run.py
+++
@@ -1,9 +0,0 @@
-""" This File is used to run the app deployed or locally"""
-import os
-from app import APP
-
-port = int(os.environ.get('PORT', 33506))
-# to run local
-#APP.run(debug=APP.config['DEBUG'], use_reloader=APP.config['RELOADER'])
-# run deployed
-APP.run(debug=APP.config['DEBUG'], use_reloader=APP.config['RELOADER'], port=port, host='0.0.0.0')
