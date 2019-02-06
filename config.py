3e5291dffb1a5a0992e4eff1daae90033f616243
IldikoMagda <ildiko.magda@gmail.com>
2019-02-06 12:43:50 +0000

    process file changed to top results, added bargraph to results, html changed accordingly

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ DEBUG = True    # Turns on debugging features in Flask
 RELOADER = True
 UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
 DOWNLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/downloads'
+STATIC_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/static'
