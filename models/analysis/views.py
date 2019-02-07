449a054881e0ebe8e00ffefd4fde03cc53b93b6f
IldikoMagda <ildiko.magda@gmail.com>
2019-02-07 16:27:39 +0000

    Still trying to revert to the working file upload

--- models/analysis/views.py
+++ models/analysis/views.py
@@ -1,7 +1,7 @@
 
 """This file is to run the essential functions of the analysis site """
 import os
-from flask import Blueprint, render_template, request, url_for, redirect
+from flask import Blueprint, render_template, request, url_for, redirect, send_from_directory
 from werkzeug.utils import secure_filename
 import config
 from models.analysis import process_file
@@ -14,8 +14,10 @@ __author__ = "Ildiko"
 
 analysis_blueprint = Blueprint('analysis', __name__) #bluprint to wrap up the code
 
-
-UPLOAD_FOLDER = config.UPLOAD_FOLDER #contains abs path to uploaded file
+#contains abs path to uploaded file
+UPLOAD_FOLDER = config.UPLOAD_FOLDER 
+#the folder where the results can be saved-the full csv
+DOWNLOAD_FOLDER = config.DOWNLOAD_FOLDER
 
 ALLOWED_EXTENSIONS = {'tsv'} #specify the file extension allowed
 def allowed_file(filename):
@@ -32,7 +34,6 @@ def index():
 @analysis_blueprint.route('upload/', methods=['POST', 'GET'])
 def upload():
     """This function uploads the file to uploads folder """
-
     if request.method == 'POST':
         uploadedfile = None
         file = request.files['file']        
@@ -49,11 +50,26 @@ def uploaded():
     """This function maybe runs the analysis """
     # take the file and analise
     result_object = process_file.actual_analysis()
+    #create csv in dowload folder
+    #process_file.create_csv(result_object)
+
     #delete the files in the upload folder
-    process_file.delete_foldercontent()
-    #create our precious picture 
+    #process_file.delete_foldercontent()
+    #create our precious picture
     ourprecious = process_file.create_fancybargraph(result_object)
 
     return render_template("analysis/results.html",
-                            tables=[result_object.to_html(classes='data', header="true")],
-                            ourprecious=ourprecious)
+                           tables=[result_object.to_html(classes='data', header="true")],
+                           ourprecious=ourprecious)
+
+@analysis_blueprint.route('uploaded/s', methods=['GET'])
+def saveresults(filename):
+    """This is the function which dowloads the results for the user"""
+    if request.method=='GET':
+        fname = DOWNLOAD_FOLDER
+        if fname is not None and os.path.isfile(fname):
+        
+            return send_from_directory(fname, filename, as_attachment=True)
+        else:
+            return redirect (url_for("analysis/index.html"))
+
