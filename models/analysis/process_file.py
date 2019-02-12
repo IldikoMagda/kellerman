f9a3fae4b65537db40b2e25aa4ef67fdbc94fe78
IldikoMagda <ildiko.magda@gmail.com>
2019-02-12 20:28:49 +0000

    Changes to analysis Connor made, first version, worker removed

--- models/analysis/process_file.py
+++ models/analysis/process_file.py
@@ -8,12 +8,9 @@ import matplotlib as mat
 mat.use('agg')
 import matplotlib.pyplot as plt
 import psycopg2 as pg
-import pandas.io.sql as psql
+
 import numpy as np
 import math
-from rq import Queue
-from worker import conn
-q = Queue(connection=conn)
 
 import config
 
@@ -46,193 +43,180 @@ def actual_analysis():
     """ this is where the analysis of the uploaded tsv file run"""
 
     __author__ = "Connor and Ildiko "
-    # let's try to send this entire function to a background process
-
-
-    # setting input file as a variable to be easily used across the script
-    input_file =process_file()
-
-    # connect to our database and retrieve the necessary info
-    connection = pg.connect("dbname=d71uh4v1fd2hq user=tdsneouerzmxkj password=92a500cb091fe70168b32c66fa6a3d6c376d467d57fb9b663eb5d13446ecb2e6 host=ec2-54-75-245-94.eu-west-1.compute.amazonaws.com")
-    dataframe = psql.read_sql('SELECT * FROM public."Phosphosite_table"', connection)
-    dataframe = dataframe[["GENE_NAME", "PHOSPHORYLATION_LOC", "KINASE_NAME"]]
-
-    counter = 0
+    
+    pathjoin = os.path.join
     # lists to extract the specific information that i want
     names = []
     residuelist = []
     kinase = []
     fclist = []
 
-    # basically cleaning the raw data to make it easier to use
-    with process_file() as data:           # this might not work as double opening
+    for file in  os.listdir(UPLOAD_FOLDER):
+        data = open(pathjoin(UPLOAD_FOLDER, file), 'r')
         data = csv.reader(data, delimiter='\t')
         next(data, None)
         for row in data:
-            counter = counter + 1
             data = row[0:6]
+        if "(" in data[0]:
             name = data[0].split("(")[0]
             residue1 = data[0].split("(")[1]
             residue = residue1.split(")")[0]
             fc = data[3]
-            if "_" in name: 
-                name = name.split("_")[0]
-            for row in dataframe.itertuples():
-                namez = row[1]
-                resid = row[2]
-                kin = row[3]
-                if namez == name:
-                    if residue == resid:
-                        names.append(name)
-                        residuelist.append(resid)
-                        kinase.append(kin)
-                        fclist.append(fc)
-    # putting into pandas df
-    df = pd.DataFrame()
-    df["Names"] = names
-    df["Residue"] = residuelist
-    df["Kinase"] = kinase
-    df["Fold Change"] = fclist 
+        else:
+            pass
+        if "_" in name: 
+            name = name.split("_")[0]  
+        if residue != "None":
+            if residue != "inf":
+                names.append(name)
+                residuelist.append(residue)
+                fclist.append(fc)
 
-    # part two starts here
+        # putting into pandas df
+        df2 = pd.DataFrame()
+        df2["gene"] = names
+        df2["residue"] = residuelist
+        df2["fold_change"] = fclist      
 
-    log2 = []
-    df = df[df != 'inf']
-    df = df[df != '0']
-    df = df[df != '']
-    df_cleaned = df.dropna(how='all')
-    df_cleaned2 = df_cleaned.dropna()
-    # again, cleaning the data
-    # calcualting the log2 for the substrates
-    for row in df_cleaned2.itertuples():
-        name = row[1]
-        residue = row [2]
-        kinase = row[3]
-        fc = float(row[4])
-        twoval = math.log(fc, 2)
-        log2.append(twoval)
-    # creating a new column containing the log2(fc)
-    df_cleaned2["log2(fc)"] = log2
+        genename = names
+        location = residuelist
 
-    # calculating log2(fc) specific to kinases
-    total = {}
+        connection = pg.connect("dbname=d71uh4v1fd2hq user=tdsneouerzmxkj \
+        password=92a500cb091fe70168b32c66fa6a3d6c376d467d57fb9b663eb5d13446ecb2e6 \
+        host=ec2-54-75-245-94.eu-west-1.compute.amazonaws.com")
 
-    for row in df_cleaned2.itertuples():
-        name = row[1]
-        residue = row [2]
-        kinase = row[3]
-        fc = float(row[4])
-        log2fc = float(row[5])
-        if kinase == kinase:
-            if kinase in total:
-                total[kinase].append(log2fc)
-            else:
-                total[kinase] = [log2fc]
+        cur = connection.cursor()
 
-    # taking the mean value
-    newdict = {}
+        result = cur.execute('SELECT "KINASE_NAME", "GENE_NAME", "RESIDUE" FROM public."Phosphosite_table" WHERE "GENE_NAME" LIKE any(%s) and "RESIDUE" LIKE any(%s)',\
+                (genename, location))
 
-    for key, value in total.items():
-        s = sum(value)/len(value)
-        newdict[key] = s 
+        df = pd.DataFrame(columns=['kinase','gene','residue'])
+        df = pd.DataFrame(cur.fetchall())
+        #df.columns = ['kinase', 'gene', 'residue']
 
-    #open file and read with pandas with delimiter
-    #rdf = pd.read_csv((process_file()), delim_whitespace=True)
+        connection.close()
 
-    rdf = pd.read_csv(input_file, delim_whitespace=True)     
-    nrdf = rdf.iloc[:,0:4]
-    new = nrdf.replace(0, np.nan)
-    new = new.dropna(how='all')
-    new = new.dropna()
+        result = pd.merge(df, df2, on = ('gene','residue'), how = "left")
 
-    # calcuating total log2(fc) mean value for all phosphosites
-    another = []
+        result['fold_change'].dropna()
+        d = result.replace("inf", np.nan)
+        e = d.replace("0", np.nan)
+        f = e.replace("", np.nan)
+        new = f.dropna(how='all')
+        df3 = new.dropna()
+        log2 = []
+        for row in df3.itertuples():
+            name = row[1]
+            residue = row [2]
+            kinase = row[3]
+            fc = float(row[4])
+            twoval = math.log(fc, 2)
+            log2.append(twoval)
 
-    for d in new.itertuples():
-        fc = str(d[4])
-        if fc != 'inf':
-            fc = math.log(float(fc), 2)
-            another.append(fc)
+        df3["log2(fc)"] = log2
+
+        total = {}
+        for row in df3.itertuples():
+            name = row[2]
+            residue = row [3]
+            kinase = row[1]
+            fc = float(row[4])
+            log2fc = float(row[5])
+            if kinase == kinase:
+                if kinase in total:
+                    total[kinase].append(log2fc)
+                else:
+                    total[kinase] = [log2fc]
+
+        newdict = {}
+
+        for key, value in total.iteritems():
+            s = sum(value)/len(value)
+            newdict[key] = s 
+
+        rdf = pd.read_csv(data, sep="\t")     
+        nrdf = rdf.iloc[:,0:4]
+        new = nrdf.replace(0, np.nan)
+        new = new.dropna(how='all')
+        new = new.dropna()
+
+        another = []
+
+        for d in new.itertuples():
+            fc = str(d[4])
+            if fc != 'inf':
+                fc = math.log(float(fc), 2)
+                another.append(fc)
+                
+        p = sum(another)/len(another)
+
+        m = math.sqrt(len(df3.index))
+
+        s = np.std(another)
+
+        score = {}
+
+        for key, value in newdict.iteritems():
+            val = ((value - p)*m)/float(s)
+            score[key] = val
+
+        # putting values into a pandas df    
             
-    p = sum(another)/len(another)
+        kinase = []
+        scores = []
 
-    # total number of phosphosites specific to kinases, square rooted
+        for key, value in score.iteritems():
+            kinase.append(key)
+            scores.append(value)
 
-    m = math.sqrt(len(df_cleaned2.index))
+        final = pd.DataFrame()
+        final["Kinase"] = kinase
+        final["z-score"] = scores
 
-    # mean log2(fc) of all phosphosites 
+        # putting kinases in order from lowest to highest z-score
 
-    s = np.std(another)
+        organised = final.sort_values(['z-Score'], ascending = True) 
 
-    # allocating the z-scores to their kinase
+        # indexing top ten z-scores
 
-    score = {}
+        top10 = organised.iloc[0:11,:]
 
-    for key, value in newdict.iteritems():
-        val = ((value - p)*m)/float(s)
-        score[key] = val
+        # saves data into csv files
+        organised.to_csv('total_list.csv')
+        top10.to_csv('topten_list.csv')
 
-    # putting values into a pandas df
-    kinase = []
-    scores = []
-
-    for key, value in score.iteritems():
-        kinase.append(key)
-        scores.append(value)
-        
-    final = pd.DataFrame()
-    final["Kinase"] = kinase
-    final["z-Score"] = scores
-
-    # putting kinases in order from lowest to highest z-score
-
-    organised = final.sort_values(['z-Score'], ascending = True) 
-
-    # indexing top ten z-scores
-
-    top10 = organised.iloc[0:11,:]
-
-    # saves data into csv files
-    organised.to_csv('total_list.csv')
-    top10.to_csv('topten_list.csv')
-
-    organised.to_csv(os.path.join(DOWNLOAD_FOLDER, r'total_list.csv'),
-                                 sep='\t', encoding='utf-8')
-    top10.to_csv(os.path.join(DOWNLOAD_FOLDER, r'topten_list.csv'),
-                                 sep='\t', encoding='utf-8')
-    return top10
+        organised.to_csv(os.path.join(DOWNLOAD_FOLDER, r'total_list.csv'),
+                                        sep='\t', encoding='utf-8')
+        top10.to_csv(os.path.join(DOWNLOAD_FOLDER, r'topten_list.csv'),
+                                        sep='\t', encoding='utf-8')
+        return top10
 
 def create_fancybargraph(objectfromanalysis):
     """ this function uses matplotlib to create a graphical presentation about the uploaded file"""
 
-    __author__="Connor"    
-    funny_loop = True
-    #putting values into a bar graph
-    job = q.enqueue_call(process_file, args=None , timeout='1h')
-    results = job.result
-    if results != None:
-        ax = results.plot.bar(x = "Kinase", rot=0, figsize=(7,3.5))
-        # adjusting figure size
-        plt.subplots_adjust(bottom=0.35, left=0.35)
-        # setting title
-        ax.set_title('Top Ten Downregulated Kinases')
-        # removing figure legend
-        ax.get_legend().remove()
-        # setting y-label
-        ax.set_ylabel('z-score')
-        # adjusting x-axis labels to be more visible
-        plt.setp(ax.get_xticklabels(), rotation=30, horizontalalignment='right')
-        # converts into figure
-        fig = ax.get_figure()
-        # saves figure
+    __author__="Connor"
 
-        fig.savefig(os.path.join((STATIC_FOLDER), 'top_ten.png'))
-        fig.savefig(os.path.join((DOWNLOAD_FOLDER),'top_ten.png'))
-        ourprecious = 'top_ten.png'
-        funny_loop == False
-        return ourprecious
-    else: 
-        funny_loop == True
+
+    ax = objectfromanalysis.plot.bar(x= "Kinase", rot=0, figsize=(7,3.5))
+    # adjusting figure size
+    plt.subplots_adjust(bottom=0.35, left=0.35)
+    # setting title
+    ax.set_title('Top Ten Downregulated Kinases')
+    # removing figure legend
+    ax.get_legend().remove()
+    # setting y-label
+    ax.set_ylabel('z-score')
+    # adjusting x-axis labels to be more visible
+    plt.setp(ax.get_xticklabels(), rotation=30, horizontalalignment='right')
+    # converts into figure
+    fig = ax.get_figure()
+    # saves figure
+
+    fig.savefig(os.path.join((STATIC_FOLDER), 'top_ten.png'))
+    fig.savefig(os.path.join((DOWNLOAD_FOLDER),'top_ten.png'))
+    ourprecious = 'top_ten.png'
+
+    return ourprecious
 
 def delete_foldercontent():
     """This function might be able to delete data from our upload folder"""
